6485fde7e4f5aae97f40c59bef68c12d99ef32e1
RokasVaitkevicius <vaitkevicius.rokas@gmail.com>
2023-11-09 01:06:35 +0200

    Client: added proper user uuid generation function

--- client-website/user-tracker.js
+++ client-website/user-tracker.js
@@ -1,16 +1,32 @@
+function uuid4() {
+  let array = new Uint8Array(16)
+  crypto.getRandomValues(array)
+
+  // Manipulate the 9th byte
+  array[8] &= 0b00111111 // Clear the first two bits
+  array[8] |= 0b10000000 // Set the first two bits to 10
+
+  // Manipulate the 7th byte
+  array[6] &= 0b00001111 // Clear the first four bits
+  array[6] |= 0b01000000 // Set the first four bits to 0100
+
+  const pattern = "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"
+  let idx = 0
+
+  return pattern.replace(
+    /XX/g,
+    () => array[idx++].toString(16).padStart(2, "0") // padStart ensures a leading zero, if needed
+  )
+}
+
 ;(function () {
   const endpoint = "http://localhost:8080/visits"
   const localStorageKey = "user_identifier"
 
-  // Function to generate a unique identifier
-  function generateIdentifier() {
-    return "uid_" + Math.random().toString(36).substr(2, 9)
-  }
-
   // Get or create the user identifier
   let userIdentifier = localStorage.getItem(localStorageKey)
   if (!userIdentifier) {
-    userId = generateIdentifier()
+    userIdentifier = uuid4()
     localStorage.setItem(localStorageKey, userIdentifier)
   }
 
