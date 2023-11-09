ff9a6493d69cdd4fefdfad2eba79d9f5a89d8666
RokasVaitkevicius <vaitkevicius.rokas@gmail.com>
2023-11-09 19:51:30 +0200

    Dashboard: multi-tenancy update

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -4,20 +4,20 @@ import "./App.css"
 function App() {
   const [visits, setVisits] = useState([])
   const [aggregation, setAggregation] = useState("minute")
+  const [org, setOrg] = useState("2agp59nwu8nrszm4p6kfriekoeo0s1")
 
   useEffect(() => {
-    fetch(`http://localhost:8080/visits?aggregation=${aggregation}`)
+    fetch(`http://localhost:8080/visits?aggregation=${aggregation}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": org,
+      },
+    })
       .then((response) => response.json())
       .then((data) => setVisits(data.data))
       .catch((error) => console.error("Error fetching visits:", error))
-  }, [aggregation])
-
-  useEffect(() => {
-    fetch("http://localhost:8080/visits")
-      .then((response) => response.json())
-      .then((data) => setVisits(data.data))
-      .catch((error) => console.error("Error fetching visits:", error))
-  }, [])
+  }, [aggregation, org])
 
   return (
     <div className="App">
@@ -31,6 +31,11 @@ function App() {
           <option value="hour">Hour</option>
           <option value="day">Day</option>
           <option value="month">Month</option>
+        </select>
+
+        <select value={org} onChange={(e) => setOrg(e.target.value)}>
+          <option value="2agp59nwu8nrszm4p6kfriekoeo0s1">Org 1</option>
+          <option value="api-key-tenant-b">Org 2</option>
         </select>
 
         <table>
