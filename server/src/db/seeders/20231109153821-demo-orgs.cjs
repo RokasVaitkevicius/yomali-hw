d5d4afb5a1412e0a9caf5e15dd13ac0ffabf0b3e
RokasVaitkevicius <vaitkevicius.rokas@gmail.com>
2023-11-10 11:45:56 +0200

    Server: better seeder separation

--- server/src/db/seeders/20231109153821-demo-orgs.cjs
+++ server/src/db/seeders/20231109153821-demo-orgs.cjs
@@ -15,53 +15,6 @@ module.exports = {
       ],
       {}
     );
-
-    await queryInterface.bulkInsert(
-      'users',
-      [
-        {
-          identifier: '927dc1a1-1feb-427c-b4a8-77f50da251bf',
-          org_id: 1,
-        },
-        {
-          identifier: '9c90113e-ed23-44dc-a2bb-e7390edfbd95',
-          org_id: 2,
-        },
-      ],
-      {}
-    );
-
-    const visits1 = [];
-    const visits2 = [];
-
-    // random 10 page names array
-    const pageNames = ['home', 'about', 'contact', 'pricing', 'careers', 'blog', 'faq', 'support', 'features', 'docs'];
-
-    for (let i = 0; i < 1000; i++) {
-      const now = new Date();
-      const startOfMonth = new Date(now.getFullYear(), now.getMonth(), 1);
-      const endOfMonth = new Date(now.getFullYear(), now.getMonth() + 1, 0);
-      const randomDate = new Date(
-        startOfMonth.getTime() + Math.random() * (endOfMonth.getTime() - startOfMonth.getTime())
-      );
-
-      visits1.push({
-        user_id: 1,
-        org_id: 1,
-        page_url: `http://example.com/${pageNames[Math.floor(Math.random() * pageNames.length)]}`,
-        visited_at: randomDate,
-      });
-
-      visits2.push({
-        user_id: 2,
-        org_id: 2,
-        page_url: `http://example.com/${pageNames[Math.floor(Math.random() * pageNames.length)]}`,
-        visited_at: randomDate,
-      });
-    }
-
-    await queryInterface.bulkInsert('visits', visits1, {});
-    await queryInterface.bulkInsert('visits', visits2, {});
   },
 
   async down(queryInterface) {
